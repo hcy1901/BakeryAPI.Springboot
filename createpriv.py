224e0c02bb8b15440b04d83aa3b78c8146d1bdc5
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2020-05-16 23:03:24 +0000

    change desc

--- createpriv.py
+++ createpriv.py
@@ -1,4 +1,4 @@
 import binascii
 
 from web3.auto import w3
-with open("ropsten/keystore/UTC--20
+with open("ropsten/keystore/UTC--2019-07-02T09-52-48.427220854Z--ed02d134589abc166c10990a015e53b6ac14a277")
