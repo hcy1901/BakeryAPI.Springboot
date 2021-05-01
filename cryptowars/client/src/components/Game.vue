5882421ecccd02251a7f12fee901b5f984f40262
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2021-05-01 08:32:07 +0000

    Updated HISTORY

---
+++ cryptowars/client/src/components/Game.vue
@@ -0,0 +1,48 @@
+
+<template>
+    <swiper ref="mySwiper" :options="swiperOptions">
+        <swiper-slide class="swiper-margin no-swipe">
+            <v-container fill-height>
+                <v-layout>
+                    <StartPage v-on:go="nextSlide()"/>
+                </v-layout>
+            </v-container>
+        </swiper-slide>
+        <swiper-slide class="swiper-margin no-swipe">
+            <v-layout text-xs-center wrap>
+                <v-flex xs12>
+                    <GameOpen
+                        :timer="timer"
+                        v-on:player-chosen="setPlayer"
+                        v-on:move-chosen="setMove"
+                        v-on:play="userPlay"
+                        v-on:timer-end="gameTimerEnd"
+                    />
+                </v-flex>
+            </v-layout>
+        </swiper-slide>
+        <swiper-slide class="swiper-margin no-swipe">
+            <GameClosed
+                v-if="game"
+                :game="game"
+                :timer="timer"
+                :player="player"
+                :move="move"
+                :winningPayment="winningPayment"
+                v-on:timer-end="resolveTimerEnd"
+                v-on:restart-game="restartGame()"
+            />
+        </swiper-slide>
+
+        <!-- <v-btn absolute small top left fab
+            color="white"
+            slot="button-prev"
+            class="nav prev"
+        >
+            <v-icon>fa-chevron-left</v-icon>
+        </v-btn>
+        <v-btn absolute small top right fab
+            color="white"
+            class="nav next"
+            slot="button-next"
+        >
