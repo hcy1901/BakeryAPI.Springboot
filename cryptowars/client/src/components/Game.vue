425ffabf16cc611db84f5580d690d5f3bb20197e
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2021-05-02 06:13:37 +0000

    On branch master Changes to be committed:

--- cryptowars/client/src/components/Game.vue
+++ cryptowars/client/src/components/Game.vue
@@ -112,3 +112,94 @@ export default {
         },
     },
     watch: {
+        userInfo() {
+            this.setUserRaidenApi();
+        },
+    },
+    mounted() {
+        this.setUserRaidenApi();
+    },
+    methods: {
+        setUserRaidenApi() {
+            this.userRaidenApi = new UserRaidenApi(
+                Vue.axios,
+                this.userInfo.ip,
+                GameGuardian.token_address[Network],
+                GameGuardian.raiden_address[Network]
+            );
+        },
+        restartGame() {
+            this.swiper.slideTo(0, 1000, false);
+        },
+        nextSlide() {
+            if (this.swiper.realIndex == 0) {
+                console.log('this.userInfo', this.userInfo);
+                this.game = null;
+                this.raiden_payment = null;
+                this.winningPayment = null;
+                this.player = null;
+                this.move = null;
+                this.moveStarted = null;
+                this.secret = null;
+                if (!this.userInfo.address || !this.userInfo.ip) {
+                    this.$emit('needs-info');
+                    return;
+                }
+                this.setCurrentGame().then(() => {
+                    if (this.gameState == GameState.resolved) {
+                        console.log('gameState resolved, starting new game');
+                        return this.startGame();
+                    }
+                    return;
+                }).then(() => {
+                    return this.setCurrentGame();
+                }).then(() => {
+                    if (this.gameState == GameState.closed) {
+                        alert(`wait for results on previous game: ${Math.floor(this.wait / 1000)} sec`);
+                    } else {
+                        this.swiper.slideNext(1000, false);
+                    }
+                });
+            } else {
+                this.swiper.slideNext(1000, false);
+            }
+        },
+        userPlay() {
+            let self = this;
+            if (!this.player || !this.move) {
+                alert('Choose a player and a move.');
+            }
+
+            async function play() {
+                if (!self.paymentIdentifier) {
+                    await self.initMove();
+                }
+                if (!self.paymentIdentifier) {
+                    throw new Error('Payment identifier not received.')
+                }
+                self.userRaidenApi.pay({
+                    amount: GameGuardian.amount,
+                    identifier: self.paymentIdentifier,
+                }).then((response) => {
+                    console.log('raiden payment response', response);
+                    self.paymentIdentifier = null;
+                    self.raiden_payment = response.data;
+                    self.nextSlide();
+                }).catch((error) => {
+                    alert(`${error} on ${self.userRaidenApi.ip}`);
+                });
+            };
+            play();
+        },
+        initMove() {
+            return this.guardianApi.initMove(this.game._id, {
+                playerId: String(this.player),
+                userAddress: this.userInfo.address,
+                moveHash: this.getMoveHash(),
+            }).catch((error) => {
+                alert(`${error} on ${this.guardianApi.ip}`);
+            }).then((response) => {
+                this.moveStarted = response.data;
+                this.paymentIdentifier = response.data.paymentIdentifier;
+            });
+        },
