89006b7745b5f4e552d2005dcda149a204ed4da4
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2021-05-08 22:47:26 +0000

    Update to the version 1.1.5

--- cryptowars/client/src/components/GameClosed.vue
+++ cryptowars/client/src/components/GameClosed.vue
@@ -4,4 +4,6 @@
             <v-flex xs12 v-if="winningMove">
                 <v-btn
                     large fab
-      
+                >
+                    <v-icon x-large>{{ `fa-hand-${winningMove}` }}</v-icon>
+  
