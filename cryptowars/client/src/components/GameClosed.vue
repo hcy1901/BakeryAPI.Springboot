917422fc7240794cbb9e618706db0dbca1c4a190
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2021-05-08 15:10:18 +0000

    doc: fix markup

--- cryptowars/client/src/components/GameClosed.vue
+++ cryptowars/client/src/components/GameClosed.vue
@@ -1,4 +1,7 @@
 <template>
     <v-container fill-height>
         <v-layout text-xs-center wrap>
-         
+            <v-flex xs12 v-if="winningMove">
+                <v-btn
+                    large fab
+      
