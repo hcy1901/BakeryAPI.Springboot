e43c811fd4926c8f3868be412f44bcc0812963c8
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2021-05-09 05:30:10 +0000

    Added more files.

--- cryptowars/client/src/components/GameClosed.vue
+++ cryptowars/client/src/components/GameClosed.vue
@@ -6,4 +6,5 @@
                     large fab
                 >
                     <v-icon x-large>{{ `fa-hand-${winningMove}` }}</v-icon>
-  
+                </v-btn>
+                <p class="display-2">{{ `You ${MovesToIn
