8dfa591f3fb29d7e67a136424a427fe45aaf7f4f
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2021-06-28 20:11:24 +0000

    Bit of refactoring

---
+++ cryptowars/client/src/components/HelpMenu.vue
@@ -0,0 +1,14 @@
+
+<template>
+    <v-dialog ref="myDialog"
+        v-model="dialog"
+        width="500"
+        persistent
+    >
+        <v-btn absolute small top right fab
+            color="white"
+            class="menu help"
+            slot="activator"
+        >
+            <v-icon>fa-question</v-icon>
+        </v-btn>
