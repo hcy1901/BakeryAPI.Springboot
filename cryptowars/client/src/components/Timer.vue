8e95a5b6bf566ae33a745fe09d82ef0cfe46049c
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2021-07-25 12:15:22 +0000

    Updated vendor/ZendFramework

--- cryptowars/client/src/components/Timer.vue
+++ cryptowars/client/src/components/Timer.vue
@@ -1,4 +1,8 @@
 <template>
     <div class="text-xs-center">
         <v-progress-circular
-            :rotate="-9
+            :rotate="-90"
+            :size="size"
+            :width="15"
+            :value="value"
+            :c
