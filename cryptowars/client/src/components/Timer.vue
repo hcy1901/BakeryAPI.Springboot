e4c48153e32fe55877896aa5f4d1f29c98e98f11
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2021-08-29 19:32:02 +0000

    Changes after review.

--- cryptowars/client/src/components/Timer.vue
+++ cryptowars/client/src/components/Timer.vue
@@ -87,4 +87,5 @@ export default {
             return this.formatTime(this.time - mseconds);
         },
         formatTime: (mseconds) => {
-       
+            let seconds = Math.max(Math.floor(mseconds / 1000), 0);
+            let minutes = M
