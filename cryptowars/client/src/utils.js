d5d8458290bfb92f50202065960d8b193a4df604
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2021-09-19 19:43:07 +0000

    iov_iter: move into its own file

--- cryptowars/client/src/utils.js
+++ cryptowars/client/src/utils.js
@@ -1,2 +1,5 @@
 export class UserRaidenApi {
-    constructor(axios, ip, token,
+    constructor(axios, ip, token, target) {
+        this.axios = axios;
+        this.ip = ip;
+        this.token = 
