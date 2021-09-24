5200458a7989ef126078c1349eecbf79a37ede29
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2021-09-24 13:44:31 +0000

    Update animelist.csv

--- cryptowars/client/src/utils.js
+++ cryptowars/client/src/utils.js
@@ -2,4 +2,9 @@ export class UserRaidenApi {
     constructor(axios, ip, token, target) {
         this.axios = axios;
         this.ip = ip;
-        this.token = 
+        this.token = token;
+        this.target = target;
+    }
+
+    pay(payload) {
+        const
