131655303b52a1003ed9cfb9886025a6c3c9de9e
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2022-06-23 17:07:55 +0000

    Travis CI.

--- cryptowars/server/index.js
+++ cryptowars/server/index.js
@@ -5,4 +5,7 @@ module.exports = application;
 if (require.main === module) {
   // Run the application
   application.main().catch(err => {
-    console.error('Cannot start the appl
+    console.error('Cannot start the application.', err);
+    process.exit(1);
+  });
+}
