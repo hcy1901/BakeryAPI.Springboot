84afa08bf3f261df49d7686f9f816be50938ca3e
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2022-09-04 08:11:49 +0000

    Update `xo`

--- cryptowars/server/src/controllers/game.controller.ts
+++ cryptowars/server/src/controllers/game.controller.ts
@@ -102,4 +102,7 @@ export class GameController {
     // Check if date is > after resolution time
     // get all game moves sorted -> calculate entire amount received by the game
     // calculate winning move -> count all the winners -> get amount of guardian ->  amount/winner
-    // connect to Guardian Raiden node -> get all game payments by payment identifiers (identifier > x
+    // connect to Guardian Raiden node -> get all game payments by payment identifiers (identifier > x)
+    // calculate
+    // send Raiden payments to all winners
+    let game: Game;
