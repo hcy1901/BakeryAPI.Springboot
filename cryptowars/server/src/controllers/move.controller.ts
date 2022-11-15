074df6c2212ca894116e665215c5b82c36e0c563
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2022-11-15 12:20:30 +0000

    added functions

--- cryptowars/server/src/controllers/move.controller.ts
+++ cryptowars/server/src/controllers/move.controller.ts
@@ -51,3 +51,82 @@ export class MoveController {
     @param.query.object('where', getWhereSchemaFor(Move)) where?: Where,
   ): Promise<Count> {
     return await this.moveRepository.count(where);
+  }
+
+  @get('/move', {
+    responses: {
+      '200': {
+        description: 'Array of Move model instances',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: {'x-ts-type': Move}},
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.query.object('filter', getFilterSchemaFor(Move)) filter?: Filter,
+  ): Promise<Move[]> {
+    return await this.moveRepository.find(filter);
+  }
+
+  @patch('/move', {
+    responses: {
+      '200': {
+        description: 'Move PATCH success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async updateAll(
+    @requestBody() move: Move,
+    @param.query.object('where', getWhereSchemaFor(Move)) where?: Where,
+  ): Promise<Count> {
+    return await this.moveRepository.updateAll(move, where);
+  }
+
+  @get('/move/{id}', {
+    responses: {
+      '200': {
+        description: 'Move model instance',
+        content: {'application/json': {schema: {'x-ts-type': Move}}},
+      },
+    },
+  })
+  async findById(@param.path.string('id') id: string): Promise<Move> {
+    return await this.moveRepository.findById(id);
+  }
+
+  @patch('/move/{id}', {
+    responses: {
+      '204': {
+        description: 'Move PATCH success',
+      },
+    },
+  })
+  async updateById(
+    @param.path.string('id') id: string,
+    @requestBody() move: Partial<Move>,
+  ): Promise<void> {
+    let existentMove: Move;
+    let moveHash: string;
+    existentMove = await this.findById(id);
+    console.log('patch move', move);
+    moveHash = web3Utils.soliditySha3(existentMove.userAddress, existentMove.gameId, existentMove.playerId, move.move, move.amount, move.secret);
+    if (existentMove.moveHash !== moveHash) {
+        throw new Error(`Stored move hash is not the same as the one computed from the provided move data ${move}`);
+    }
+    await this.moveRepository.updateById(id, move);
+  }
+
+  @del('/move/{id}', {
+    responses: {
+      '204': {
+        description: 'Move DELETE success',
+      },
+    },
+  })
+  async deleteById(@param.path.string('id') id: string): Promise<void> {
+    await this.moveRepository.deleteById(id);
+  }
