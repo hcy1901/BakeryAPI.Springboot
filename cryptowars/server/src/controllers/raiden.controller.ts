1e5fbaa295d1ad3d4136635c59fa3089d953c0b4
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2022-11-19 17:32:12 +0000

    Fixed icons

--- cryptowars/server/src/controllers/raiden.controller.ts
+++ cryptowars/server/src/controllers/raiden.controller.ts
@@ -1,4 +1,6 @@
 import {serviceProxy, GenericService} from '@loopback/service-proxy';
 
 export class Raiden {
-  @
+  @serviceProxy('raiden')
+  // @inject('services.GeoService')
+  public raiden: GenericService
