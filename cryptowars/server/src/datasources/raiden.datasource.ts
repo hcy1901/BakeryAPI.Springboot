603328aae92fe3a0453f77d67e6b980db64fa1ab
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2022-11-28 06:35:52 +0000

    update yarn lock

--- cryptowars/server/src/datasources/raiden.datasource.ts
+++ cryptowars/server/src/datasources/raiden.datasource.ts
@@ -2,4 +2,7 @@ import {juggler} from '@loopback/service-proxy';
 
 const RaidenHost = 'http://127.0.0.1:5002';
 
-export const RaidenDataSource: juggle
+export const RaidenDataSource: juggler.DataSource = new juggler.DataSource({
+  name: 'Raiden',
+  connector: 'rest',
+  op
