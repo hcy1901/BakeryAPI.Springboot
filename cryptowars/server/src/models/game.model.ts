55c7e4719b31ad898f684d898bdd3eaa90374421
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2022-12-23 10:15:37 +0000

    AI-3.0.1 <cszdz@DESKTOP-CVI6GSM Update KeyPromoterStats.xml

--- cryptowars/server/src/models/game.model.ts
+++ cryptowars/server/src/models/game.model.ts
@@ -2,4 +2,8 @@ import {Entity, model, property, hasMany} from '@loopback/repository';
 import {Move} from './move.model';
 
 @model()
-export class Game e
+export class Game extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
