d1effaf31ac7c525ba4b33422872252b0e30944e
hcy1901 <112608594+hcy1901@users.noreply.github.com>
2023-01-08 16:39:16 +0000

    fixed names

--- cryptowars/server/src/models/move.model.ts
+++ cryptowars/server/src/models/move.model.ts
@@ -8,3 +8,59 @@ export class Move extends Entity {
     id: true,
     generated: true,
   })
+  _id: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  playerId: string;
+
+  @property({
+    type: 'string',
+  })
+  gameId: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  userAddress: string;
+
+  @property({
+    type: 'number',
+  })
+  paymentIdentifier: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  moveHash: string;
+
+  @property({
+    type: 'string',
+  })
+  move?: string;
+
+  @property({
+    type: 'string',
+  })
+  secret?: string;
+
+  @property({
+    type: 'number',
+  })
+  amount?: number;
+
+  @property({
+    type: 'date',
+    generated: true,
+    default: new Date(),
+  })
+  timestamp: Date;
+
+  constructor(data?: Partial<Move>) {
+    super(data);
+  }
+}
